29634d28a152e97d90a4d6a57924e0884f94f4e4
Sam Bhagwat <calcsam@gmail.com>
2017-02-12 01:19:19 -0800

    attempt to scope out a mapping of classes

--- example.c
+++ example.c
@@ -17,6 +17,130 @@ const char *FILE_NAMES[] = {
   "cia-crest-files-cia-crest-archive-metadata/14_export.csv",
 };
 
+#include <string.h>
+
+const int NUM_COLUMNS = 23;
+
+
+
+COLUMN_SIZE = 150;
+
+void filter() {
+
+  return
+}
+
+void return_next_matching_row(void (*filter)(Row), Row (*get_next)(int)) {
+  Row r = get_next();
+  while(!filter(r)) {
+    r = get_next()
+  }
+  return r;
+}
+
+
+struct Executor {
+  QueryPlan qp;
+  Filesystem f;
+
+  Row get_next(){
+    char * line = NULL:
+    line = f.get_next();
+    while ((keep_reading=qp.read(&line)) != -1) {
+      line = f.get_next();
+    }
+    return line;
+  }
+};
+
+struct Row {
+  char* text;
+  char* get_column(int col) {
+    char* substr[150];
+    memcpy( substr, &text[col * 150], 150);
+    return substr;
+  }
+
+}
+
+char*
+
+typedef struct {
+  int operation (char * line, ) = // pointer to a function
+  int read(char * line) {
+
+  }
+} QueryPlan;
+
+struct Filesystem {
+  // doesn't account for case that a file might be empty
+  File openFile f;
+  int fileNum = 0;
+  void get_next_line(){
+    char * line = NULL:
+    size_t len;
+    if ((read = getline((char **)&line, &len, f)) != -1) {
+      return line;
+    } else {
+      advance_file(f)
+      if (f == -NULL) {
+        return -NULL;
+      }
+      return getline((char **)&line, &len, f));
+    }
+  }
+  const char *FILE_NAMES[] = {
+    "cia-crest-files-cia-crest-archive-metadata/test.csv",
+    "cia-crest-files-cia-crest-archive-metadata/2_export.csv",
+    "cia-crest-files-cia-crest-archive-metadata/3_export.csv",
+    "cia-crest-files-cia-crest-archive-metadata/4_export.csv",
+    "cia-crest-files-cia-crest-archive-metadata/5_export.csv",
+    "cia-crest-files-cia-crest-archive-metadata/6_export.csv",
+    "cia-crest-files-cia-crest-archive-metadata/7_export.csv",
+    "cia-crest-files-cia-crest-archive-metadata/8_export.csv",
+    "cia-crest-files-cia-crest-archive-metadata/9_export.csv",
+    "cia-crest-files-cia-crest-archive-metadata/10_export.csv",
+    "cia-crest-files-cia-crest-archive-metadata/11_export.csv",
+    "cia-crest-files-cia-crest-archive-metadata/12_export.csv",
+    "cia-crest-files-cia-crest-archive-metadata/13_export.csv",
+    "cia-crest-files-cia-crest-archive-metadata/14_export.csv",
+  };
+  void advance_file() {
+    f.close();
+    fileNum++;
+    f = fopen(FILE_NAMES[fileNum], "r")
+  }
+}
+
+void get_next(void (*filter)(int)) {
+
+  return filter()
+}
+
+
+
+
+
+
+struct Table {
+   Row
+};
+
+struct Row {
+   int account_number;
+   char *first_name;
+   char *last_name;
+   float balance;
+};
+
+struct Column {
+
+   int account_number;
+   char *first_name;
+   char *last_name;
+   float balance;
+};
+
 void filescan(int j) {
   FILE *f = fopen(FILE_NAMES[j], "r");
   char currentRow[50][300];
